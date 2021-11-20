806f221cba798992dc51a422d9eb64d9cc56b061
Yauhen Kviatkouski <kvetkovski@gmail.com>
2021-11-20 17:31:52 +0100

    feat: add benchmark

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ type PostData = {
 };
 
 module.exports.main = async (data: PostData) => {
+  const start = new Date().getTime();
   const iHerbService = new IherbService();
   const products = await iHerbService.parseUri(data.link);
   console.log("Parsed products: ", products.length);
+  console.log("ms: ", new Date().getTime() - start);
   return products;
 };
